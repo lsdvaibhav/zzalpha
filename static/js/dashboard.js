8b472ac776b11f9ee6dd19c6533b67d582eaa1f3
Vaibhav Kharatmal <67339364+lsdvaibhav@users.noreply.github.com>
2020-10-23 18:21:57 +0530

    Add files via upload

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,60 +1,71 @@
 /* globals Chart:false, feather:false */
 
 (function () {
-  'use strict'
+	'use strict'
+	// Graphs
+	// Create a new FormData instance
+	var datareq = new FormData();
+	// Create a XMLHTTPRequest instance
+	var request = new XMLHttpRequest();
+	// Set the response type
+	request.responseType = "json";
 
-  feather.replace()
+	var week = "Week 4";
+	datareq.append("week", week);
+	// request load handler (transfer complete)
+	request.addEventListener("load", function (e) {
 
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
-  
-  
-  
-  
+	if (request.status == 200) {
+		var datares = [request.response.datares];
+		console.log(datares)
+		feather.replace()
+	var ctx = document.getElementById('myChart');
+	var myChart = new Chart(ctx, {
+	  
+		type: 'bar',
+		data: {
+		  labels: [
+			'Sunday',
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday'
+		  ],
+		  datasets: [{
+					backgroundColor: "#BDEDFF",
+					borderColor: "#3BB9FF",
+					borderWidth: 2,
+					data: datares[0]
+				}]
+		},
+		options: {
+			responsive: true,
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'WEEK DATA'
+			}
+		}
+	  });
+	}
+	else {
+	  alert("error in response");
+	}
+
+	});
+	// request error handler
+	request.addEventListener("error", function (e) {
+	reset();
+	alert("error in while uploading");
+	});
+	// Open and send the request
+	request.open("post", "/weekData");
+	request.send(datareq);
+
 }())
-
 
 
