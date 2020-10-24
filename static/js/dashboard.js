0e0ee9c75a92e0b35fec937ac2858529e1c371c0
Vaibhav Kharatmal <67339364+lsdvaibhav@users.noreply.github.com>
2020-10-24 15:27:19 +0530

    Add files via upload

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,60 +1,77 @@
 /* globals Chart:false, feather:false */
 
-(function () {
+function showWeekData(week){
 	'use strict'
+	
 	// Graphs
+	
 	// Create a new FormData instance
 	var datareq = new FormData();
 	// Create a XMLHTTPRequest instance
 	var request = new XMLHttpRequest();
 	// Set the response type
 	request.responseType = "json";
-
-	var week = "Week 4";
 	datareq.append("week", week);
 	// request load handler (transfer complete)
 	request.addEventListener("load", function (e) {
 
 	if (request.status == 200) {
 		var datares = [request.response.datares];
-		console.log(datares)
+		var labelList = request.response.label;
+		var TableData = request.response.TableData;
+		document.getElementById("divTable").innerHTML=TableData;
+		
 		feather.replace()
-	var ctx = document.getElementById('myChart');
-	var myChart = new Chart(ctx, {
-	  
-		type: 'bar',
-		data: {
-		  labels: [
-			'Sunday',
-			'Monday',
-			'Tuesday',
-			'Wednesday',
-			'Thursday',
-			'Friday',
-			'Saturday'
-		  ],
-		  datasets: [{
-					backgroundColor: "#BDEDFF",
-					borderColor: "#3BB9FF",
-					borderWidth: 2,
-					data: datares[0]
-				}]
-		},
-		options: {
-			responsive: true,
-			legend: {
-				position: 'top',
+		var ctx = document.getElementById('myChart');
+		var myChart = new Chart(ctx, {
+		  
+			type: 'bar',
+			data: {
+			  labels: labelList,
+			  datasets: [{
+						backgroundColor: "#BDEDFF",
+						borderColor: "#3BB9FF",
+						borderWidth: 2,
+						data: datares[0]
+					}]
 			},
-			title: {
-				display: true,
-				text: 'WEEK DATA'
+			options: {
+				responsive: true,
+				legend: {
+					position: 'top',
+				},
+				scales: {
+						xAxes: [{
+								display: true,
+								scaleLabel: {
+									display: true,
+									labelString: 'Date'
+								}
+							}],
+						yAxes: [{
+								display: true,
+								scaleLabel: {
+									display: true,
+									labelString: 'Audit Count'
+								},
+								ticks: {
+									beginAtZero: true,
+									steps: 20,
+									stepValue: 2,
+									max: 100
+								}
+							}]
+					},
+				title: {
+					display: true,
+					text: 'WEEK DATA'
+				}
 			}
+		  });
 		}
-	  });
-	}
-	else {
-	  alert("error in response");
-	}
+		else {
+		  alert("error in response");
+		}
 
 	});
 	// request error handler
@@ -65,7 +82,12 @@
 	// Open and send the request
 	request.open("post", "/weekData");
 	request.send(datareq);
+	}
 
+(function (){
+	'use strict'
+	feather.replace()
+	
 }())
 
 
